48071f0421fcdfc22c1c82cc9c07dacf6ec48833
pawan-kavinda <138522618+pawan-kavinda@users.noreply.github.com>
2024-06-29 09:24:36 +0530

    aa

--- frontend/src/pages/StarterPage.js
+++ frontend/src/pages/StarterPage.js
@@ -12,7 +12,7 @@ const StarterPage = () => {
       <div className="text-center">
         <div className="flex flex-col items-center lg:text-[80px] text-[50px] font-bold mb-2">
           <img src={logo} alt="Logo" className="h-24 w-24" />
-          <span className='mb-8'>Beta Launch</span>
+          <span className='mb-8'>Beata Launch</span>
         </div>
         <Link to="/calculator" className="ml-6 mt-8 px-6 py-3 w-full border border-white hover:border-collapse rounded-xl mr-4 bg-gradient-to-br from-blue-950 to bg-green-600 text-white hover:border-none">Salary Calculator</Link>
       </div>
